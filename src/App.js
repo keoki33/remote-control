556fdf26a4d37cd37fe6658b583f68e7734341d9
keoki33 <gwong323@yahoo.com>
2019-06-17 19:09:12 +0100

    buttons done

--- src/App.js
+++ src/App.js
@@ -44,138 +44,158 @@ class App extends Component {
 
   render() {
     return (
-      <div className="card">
-        {console.log(this.state.volume)}
-        <h1>Remote Control</h1>
-        <button
-          className="power"
-          type="button"
-          onClick={event => {
-            this.powerButton();
-          }}
-        >
-          Power
-        </button>
+      <div className="wrapper">
+        <div className="card">
+          {console.log(this.state.volume)}
+          <img
+            className="remoteimage"
+            src={require(`./sony.jpg`)}
+            // alt=""
+            // width="104"
+            // height="34"
+          />
+          <button
+            className="power"
+            type="button"
+            onClick={event => {
+              this.powerButton();
+            }}
+          >
+            Power
+          </button>
 
-        <button
-          className="closeWindow"
-          type="button"
-          onClick={event => {
-            this.postCommand("close", "false");
-          }}
-        >
-          Close Window
-        </button>
+          <button className="blank" type="button" onClick={event => {}}>
+            blank button
+          </button>
 
-        <button
-          className="showForecast"
-          type="button"
-          onClick={event => {
-            this.postCommand("forecast", "true");
-          }}
-        >
-          Show Forecast
-        </button>
+          <button
+            className="closeWindow"
+            type="button"
+            onClick={event => {
+              this.postCommand("close", "false");
+            }}
+          >
+            Close Window
+          </button>
 
-        <button
-          className="showNews"
-          type="button"
-          onClick={event => {
-            this.postCommand("news", "true");
-          }}
-        >
-          Show News
-        </button>
+          <button
+            className="showForecast"
+            type="button"
+            onClick={event => {
+              this.postCommand("forecast", "true");
+            }}
+          >
+            Show Forecast
+          </button>
 
-        <button
-          className="emailMe"
-          type="button"
-          onClick={event => {
-            this.postCommand("email", "georgewmail-mirror@yahoo.com");
-          }}
-        >
-          Email Me
-        </button>
+          <button
+            className="showNews"
+            type="button"
+            onClick={event => {
+              this.postCommand("news", "true");
+            }}
+          >
+            Show News
+          </button>
 
-        <button
-          className="showCalendar"
-          type="button"
-          onClick={event => {
-            this.postCommand("calendar", "fullcalendarin");
-          }}
-        >
-          Show Calendar
-        </button>
+          <button
+            className="emailMe"
+            type="button"
+            onClick={event => {
+              this.postCommand("email", "georgewmail-mirror@yahoo.com");
+            }}
+          >
+            Email Me
+          </button>
 
-        <button
-          className="youtubeFullscreen"
-          type="button"
-          onClick={event => {
-            this.postCommand("youtubeSize", "videoFull");
-          }}
-        >
-          Youtube Fullscreen
-        </button>
+          <button
+            className="showCalendar"
+            type="button"
+            onClick={event => {
+              this.postCommand("calendar", "fullcalendarin");
+            }}
+          >
+            Show Calendar
+          </button>
 
-        <button
-          className="playYoutube"
-          type="button"
-          onClick={event => {
-            this.postCommand("youtube", "true");
-          }}
-        >
-          Play Youtube
-        </button>
+          <button
+            className="youtubeFullscreen"
+            type="button"
+            onClick={event => {
+              this.postCommand("youtubeSize", "videoFull");
+            }}
+          >
+            Youtube Fullscreen
+          </button>
 
-        <button
-          className="pauseYoutube"
-          type="button"
-          onClick={event => {
-            this.postCommand("youtube", "false");
-          }}
-        >
-          Pause Youtube
-        </button>
+          <button
+            className="playYoutube"
+            type="button"
+            onClick={event => {
+              this.postCommand("youtube", "true");
+            }}
+          >
+            Play Youtube
+          </button>
 
-        <button
-          className="goToSleep"
-          type="button"
-          onClick={event => {
-            this.postCommand("on", "false");
-          }}
-        >
-          Go To Sleep
-        </button>
+          <button
+            className="pauseYoutube"
+            type="button"
+            onClick={event => {
+              this.postCommand("youtube", "false");
+            }}
+          >
+            Pause Youtube
+          </button>
 
-        <button
-          className="wakeUp"
-          type="button"
-          onClick={event => {
-            this.postCommand("on", "true");
-          }}
-        >
-          Wake Up
-        </button>
+          <button
+            className="goToSleep"
+            type="button"
+            onClick={event => {
+              this.postCommand("on", "false");
+            }}
+          >
+            Go To Sleep
+          </button>
 
-        <button
-          className="volumeUp"
-          type="button"
-          onClick={event => {
-            this.volume("up");
-          }}
-        >
-          +
-        </button>
+          <button
+            className="wakeUp"
+            type="button"
+            onClick={event => {
+              this.postCommand("on", "true");
+            }}
+          >
+            Wake Up
+          </button>
+          <div className="volume">
+            <button
+              className="volumeUp"
+              type="button"
+              onClick={event => {
+                this.volume("up");
+              }}
+            >
+              +
+            </button>
 
-        <button
-          className="volumeDown"
-          type="button"
-          onClick={event => {
-            this.volume("down");
-          }}
-        >
-          -
-        </button>
+            <button
+              className="volumeDown"
+              type="button"
+              onClick={event => {
+                this.volume("down");
+              }}
+            >
+              -
+            </button>
+          </div>
+        </div>
+        <div className="logo">
+          <h1>SONY</h1>
+          <div>
+            <h3>PERSONAL MIRROR SYSTEM</h3>
+            <h4>MI-RR-3PO</h4>
+          </div>
+        </div>
       </div>
     );
   }
